f6339f7ba5934f727ea5f0bea1a70329a9e2ec3d
soondro2266 <cjy941021@gmail.com>
2025-05-16 01:45:46 +0800

    AbaloneEnv

---
+++ AbaloneEnv.h
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include "Board.h"
+#pragma once
+#define NextBoards vector<pair<Board,int>>
+
+using namespace std;
+
+class AbaloneAction{
+    public:
+    int numberofpiece;
+    int oneDpos;
+    /**
+     * one piece
+     * first :[0, 5]
+     * second :0
+     * two and three pieces
+     * first :[0, 5] the direction of otherpiece
+     * second :[-1,1]
+     */
+    pair<int, int> direction;
+    /**
+     * @brief type of action according to number of piece and second direction
+     * 
+     * `0`: 1 piece
+     * 
+     * `1`: 2 pieces ,sec_dir = -1
+     * 
+     * `2`: 2 pieces ,sec_dir = 0
+     * 
+     * `3`: 2 pieces ,sec_dir = 1
+     * 
+     * `4`: 3 pieces ,sec_dir = -1
+     * 
+     * `5`: 3 pieces ,sec_dir = 0
+     * 
+     * `6`: 3 pieces ,sec_dir = 1
+    */
+    int type = 0;
+    AbaloneAction(){}
+    void set_type();
+    void load_type();
+    int value();
+    void load_value(int value);
+};
+
+
+pair<int, int> operator+ (pair<int, int>& pos1, pair<int, int>& pos2);
+
+
+
+class AbaloneEnv
+{
+public:
+
+    AbaloneEnv(int n);
+
+    AbaloneEnv(Board board);
+
+    void load_default_setup();
+
+    // implement later
+    void load_customize_setup();
+
+    auto get_all_next_boards() -> NextBoards;
+
+    void show_current_board();
+
+    
+private:
+    // functions
+
+    void get_one_piece_Next(NextBoards& candidate);
+
+    void get_two_piece_Next(NextBoards& candidate);
+
+    void get_three_piece_Next(NextBoards& candidate);
+
+    bool is_empty(pair<int, int>& position);
+    bool is_white(pair<int, int>& position);
+    bool is_black(pair<int, int>& position);
+    bool is_ally(pair<int, int>& position);
+    bool is_enemy(pair<int, int>& position);
+    bool is_valid(pair<int, int> position);
+
+    // data
+    int number_of_edge, number_of_place;
+
+    Board currentBoard;
+    // mapping tool
+    vector<pair<int, int>> directions;
+    
+    vector<pair<int, int>> oneD_to_twoD;
+};
