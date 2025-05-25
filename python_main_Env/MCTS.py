2f821ae4398b26b5a00a096fae66648cef9191fe
soondro2266 <cjy941021@gmail.com>
2025-05-25 16:55:24 +0800

    feat: mcts

--- python_main_Env/MCTS.py
+++ python_main_Env/MCTS.py
@@ -1,4 +1,5 @@
 import os
+import json
 import torch
 import numpy as np
 from CNN import PolicyNet, ValueNet
@@ -9,59 +10,51 @@ model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "model")
 
 print(model_path)
 
-class node_info:
-    def __init__(self, action_count):
-        self.action_count = action_count
-        self.q = np.zeros((action_count, ), dtype = np.float32)
-        self.n = np.zeros((action_count, ), dtype = np.int32)
 
 class MCTS:
     def __init__(self, n = 5, eta = 1e-4, value_path = "best_model.pth", policy_path = "best_model.pth"):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.value_network = ValueNet(n).to(self.device)
         self.policy_network = PolicyNet(n).to(self.device)
-        self.value_network.load_state_dict(torch.load(model_path+"\\valueNet\\"+ value_path))
-        self.policy_network.load_state_dict(torch.load(model_path+"\\policyNet\\"+ policy_path))
+        self.value_network.load_state_dict(torch.load(model_path+"/valueNet/"+ value_path))
+        self.policy_network.load_state_dict(torch.load(model_path+"/policyNet/"+ policy_path))
+        self.mcts_record_path = model_path + "/mcts.json"
         self.value_network.eval()
         self.policy_network.eval()
         
-
-        self.place_count = 3 * n * (n - 1) + 1
         self.edge = n
-        self.action_count = self.place_count * 42
-
-        self.searching_data:dict[str, node_info] = {}
-
-        self.game = AbaloneEnv(n = n)
 
         self.eta = eta
 
-
-
-    def selection(self)->int:
-        all_possible_action = self.game.get_all_actions()
-        current_state = self.game.load_state_string()
-
-        current_info = self.searching_data.setdefault(current_state,node_info(self.action_count))
-
-        policy_net_probility = self.policy_network(self.game.get_state_tensor())
-
-        scores = []
-        for action in all_possible_action:
-            scores.append(current_info.q[action] + \
-                          self.eta * policy_net_probility[action] / (1 + current_info.n[action]))
+    def one_simulation(self, env:AbaloneEnv)->None:
+        with open(self.mcts_record_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
         
-        seleted_action = all_possible_action[np.argmax(scores)]
-        current_info.n[seleted_action] += 1
+        state_t_str = env.load_state_string()
+        if state_t_str not in data:
+            policy_net_probility = self.policy_network(env.get_state_tensor())
+            all_possible_action_t = env.get_all_actions()
+            data[state_t_str] = {}
+            for action_t in all_possible_action_t:
+                data[state_t_str][action_t] = [0, 0, 0] #pi, N, Vsum
+                data[state_t_str][action_t][0] = policy_net_probility[action]
+        
+        scores = []
+        for info in data[state_t_str].values():
+            q = 0 if info[1] == 0 else info[2]/info[1]
+            scores.append(q + \
+                          self.eta * info[0] / (1 + info[1]))
+        
+        seleted_action_t = data[state_t_str].keys()[np.argmax(scores)]
 
-        return seleted_action
-    
-    def expansion(self, action:int):
+        #selection finished
 
-        self.game.step(action) #state become state'
+        env.step(seleted_action_t)
 
-        all_possible_action_prime = self.game.get_all_actions()
-        policy_net_probility = self.policy_network(self.game.get_state_tensor())
+        #expansion start
+
+        all_possible_action_prime = env.get_all_actions()
+        policy_net_probility = self.policy_network(env.get_state_tensor())
 
         all_possible_probility = policy_net_probility[all_possible_action_prime]        
         all_possible_probility = all_possible_probility / all_possible_probility.sum()
@@ -70,15 +63,37 @@ class MCTS:
         a = dist.sample()  
         action_prime = all_possible_action_prime[a.item()]
 
-        self.game.step(action_prime) #state' become state t+1 
+        env.step(action_prime) #state' become state t+1
 
-    
+        #evaluation on state t+1
 
-         
-    
-    
-            
+        state = env.get_state_tensor()
+        valueNet_score = self.value_network(state)
 
 
+        done = env.finished
+        reward = 0
+        while not done:
+            all_possible_action = env.get_all_actions()
+            policy_net_probility = self.policy_network(state)
 
+            all_possible_probility = policy_net_probility[all_possible_action]        
+            all_possible_probility = all_possible_probility / all_possible_probility.sum()
 
+            dist = torch.distributions.Categorical(all_possible_probility)
+            a = dist.sample()  
+            action = all_possible_action[a.item()]
+
+            state, reward, done = env.step(action)
+        
+        v = 0.5 * (valueNet_score + reward)
+
+        data[state_t_str][seleted_action_t][1] += 1
+        data[state_t_str][seleted_action_t][2] += v
+
+        with open(self.mcts_record_path, 'w', encoding='utf-8') as f:
+            # ensure_ascii=False 允許中文不被轉為 \u 編碼
+            # indent=4 自動排版成易讀的格式
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+        return
